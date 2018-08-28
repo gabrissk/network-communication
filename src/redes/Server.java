2dad28060996518d0987cf1b6434c84d08c3d4e7
gabrissk <gabrielfermorais@gmail.com>
2018-08-28 19:08:27 -0300

    Exceção de porta

--- src/redes/Server.java
+++ src/redes/Server.java
@@ -38,8 +38,8 @@ public class Server {
             Message m = (Message)inputStream.readObject();
             System.out.println("Size: " +m.getSize()+"\nMessage: "+m.getMsg() +"\nHash:"+m.getMd5());
         } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
+            System.out.println("Exceção ao dar listen na porta "
+                    + portNumber);
             System.out.println(e.getMessage());
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
