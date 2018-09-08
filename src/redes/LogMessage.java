fab3cedad73ac83d9a137beb4ca0765a28783806
gabrissk <gabrielfermorais@gmail.com>
2018-09-08 13:02:53 -0300

    Timer para cada log message; logica da janela deslizante

--- src/redes/LogMessage.java
+++ src/redes/LogMessage.java
@@ -2,6 +2,7 @@ package redes;
 
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
+import java.util.Timer;
 
 public class LogMessage extends Message implements Serializable {
 
@@ -10,10 +11,13 @@ public class LogMessage extends Message implements Serializable {
     private short size;
     private String msg;
     private String md5;
+    Timer timer;
+    private boolean err;
 
     public LogMessage(long seq_num, Timestamp time, short size, String msg, String md5) {
         //super(md5);
         this.seq_num = seq_num; this.time = time; this.msg = msg; this.size = size; this.md5 = md5;
+        this.timer = new Timer(); this.err = false;
     }
 
     public long getSeq_num() { return seq_num; }
@@ -30,6 +34,10 @@ public class LogMessage extends Message implements Serializable {
 
     public String getMd5() { return this.md5; }
 
+    public boolean isErr() {
+        return err;
+    }
+
     @Override
     public String toString() {
         return "seqNum: " +seq_num+ "\tTime: " +time+
@@ -39,12 +47,22 @@ public class LogMessage extends Message implements Serializable {
     static LogMessage setAndGetMessage(String nextLine, long seq_num, double perror, Timestamp time) throws NoSuchAlgorithmException {
         String md5 = hash(String.valueOf(seq_num) + time.toString()
                 + String.valueOf((short)nextLine.length()) + nextLine);
+        LogMessage msg = new LogMessage(seq_num, time, (short)nextLine.length(), nextLine, md5);
         double rdm = Math.random();
         if(rdm < perror) {
             System.out.println("erro "+nextLine);
-            md5 = hash(md5);
+            msg.setMd5(hash(md5));
+            msg.setErr(true);
+
         }
-        return new LogMessage(seq_num, time, (short)nextLine.length(), nextLine, md5);
+        return msg;
     }
 
+    private void setMd5(String hash) {
+        this.md5 = hash;
+    }
+
+    public void setErr(boolean err) {
+        this.err = err;
+    }
 }
